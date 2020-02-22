8bc6dfae5c86d7b62cfc0ccf93053a73849dfc12
Matt Wyatt <matt@studiorepublic.com>
2020-02-22 01:04:14 +0000

    Beginning work on Colour picker + Dark / Light modes. Time for bed now though

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,76 +3,108 @@
 @section('body')
 
     <section class="home">
-        <h1 class="title">
-            hi, <span class="title__icon">👋🏼</span>
-        </h1>
+        <div class="container">
 
-        <p>
-            i'm <span title="awful at nollie flips" class="highlight">matt wyatt,</span>
-            and if i could code myself into<br>
-            my animal crossing town, i would.
-        </p>
+            <h1 class="title">
+                hi, <span class="title__icon">👋🏼</span>
+            </h1>
 
-        <aside class="divider"></aside>
+            <p>
+                i'm <span title="awful at nollie flips" class="highlight">matt wyatt,</span>
+                and if i could code myself into<br>
+                my animal crossing town, i would.
+            </p>
 
-        <ul class="list">
-            <h2 class="list__title">
-                front end developer:
-            </h2>
+            <aside class="divider"></aside>
 
-            <li class="list__item">
-                currently helping to make the web beautiful<br>
-                and the world <i>less</i> terrible at <a href="https://studiorepublic.com" title="a lovely bunch" class="link" target="_blank">Studio Republic.</a>
-            </li>
+            <ul class="list">
+                <h2 class="list__title">
+                    front end developer:
+                </h2>
 
-            <li class="list__item">
-                former <a href="https://sneekdigital.co.uk" title="making the teas" class="link" target="_blank">Sneek Digital</a>
-                bandit, now hustling at <a href="https://skybase.it" title="<3" class="link" target="_blank">Skybase.</a>
-            </li>
-        </ul>
+                <li class="list__item">
+                    currently helping to make the web beautiful<br>
+                    and the world <i>less</i> terrible at <a href="https://studiorepublic.com" title="a lovely bunch" class="link" target="_blank">Studio Republic.</a>
+                </li>
 
-        <ul class="list">
-            <h2 class="list__title">
-                afk:
-            </h2>
+                <li class="list__item">
+                    former <a href="https://sneekdigital.co.uk" title="making the teas" class="link" target="_blank">Sneek Digital</a>
+                    bandit, now hustling at <a href="https://skybase.it" title="<3" class="link" target="_blank">Skybase.</a>
+                </li>
+            </ul>
 
-            <li class="list__item">
-                skateboarding, motorbikes, running..
-            </li>
+            <ul class="list">
+                <h2 class="list__title">
+                    afk:
+                </h2>
 
-            <li class="list__item">
-                eating my vegetables
-            </li>
+                <li class="list__item">
+                    skateboarding, motorbikes, running..
+                </li>
 
-            <li class="list__item">
-                nintendo switch
-            </li>
-            <li class="list__item">
-                buying shoes
-            </li>
-        </ul>
+                <li class="list__item">
+                    eating my vegetables
+                </li>
 
-        <aside class="divider divider--thin"></aside>
+                <li class="list__item">
+                    nintendo switch
+                </li>
+                <li class="list__item">
+                    buying shoes
+                </li>
+            </ul>
 
+            <aside class="divider divider--thin"></aside>
 
-        <aside class="selfie"></aside>
+            <img
+                src="/images/selfie-alert.jpg"
+                class="selfie"
+                alt="Biggest nerd on the globe captured infront of a New York City skyline"
+                title="Well would you look at that"
+            >
 
-        <p>
-            <span class="highlight">let's chat,</span> send me an <a href="mailto:" title="or hate mail, hate mail's fine too" class="link" target="_blank">email.</a>
-        </p>
+            <p>
+                <span class="highlight">let's chat,</span> send me an <a href="mailto:" title="or hate mail lol" class="link" target="_blank">email.</a>
+            </p>
 
+            <footer class="footer">
+                <a href="https://github.com/mattie1da/world" class="link link--code" target="_blank">view the source code</a>
 
-        <footer class="footer">
-            <a href="https://github.com/mattie1da/world" class="link link--code" target="_blank">view the source code</a>
+                <div class="socials">
+                    <a href="https://twitter.com/mattie1da" class="link" target="_blank">twitter</a>
+                    <a href="https://instagram.com/mattie1da" class="link" target="_blank">instagram</a>
+                    <a href="https://github.com/mattie1da" class="link" target="_blank">github</a>
+                </div>
+            </footer>
+        </div>
 
-            <div class="socials">
-                <a href="https://twitter.com/mattie1da" class="link" target="_blank">twitter</a>
-                <a href="https://instagram.com/mattie1da" class="link" target="_blank">instagram</a>
-                <a href="https://github.com/mattie1da" class="link" target="_blank">github</a>
+        <aside class="palette">
+            <div class="themes">
+                <a href="todo" class="theme theme--one"></a>
+                <a href="todo" class="theme theme--two"></a>
+                <a href="todo" class="theme theme--three"></a>
+                <a href="todo" class="theme theme--four"></a>
+                <a href="todo" class="theme theme--five"></a>
             </div>
 
-        </footer>
+            <div class="modes">
+                <a href="todo" class="mode mode--dark"></a>
+                <a href="todo" class="mode mode--light"></a>
+            </div>
+        </aside>
 
+        {{--blob party--}}
+        <svg class="blob blob--top" xmlns="http://www.w3.org/2000/svg" width="500.79" height="369.509" viewBox="0 0 500.79 369.509">
+            <g transform="translate(-349.185 -228.221)">
+                <path d="M98.9-143.6c42,16.6,97.6,22.6,127,51,29.5,28.5,33,79.4,23.1,126.4s-33.1,90.1-68.6,111.8-83.3,22-123.3,21.2c-40.1-.8-72.4-2.7-116.3.2-43.8,2.9-99.3,10.6-128.9-11-29.7-21.6-33.6-72.6-43.3-124.2S-256.5-72-238.5-107.8c18.1-35.9,69.6-55.3,115-70.4,45.3-15.2,84.4-26,118.2-17.7S56.9-160.3,98.9-143.6Z" transform="translate(596 427)"/>
+            </g>
+        </svg>
+
+        <svg class="blob blob--bottom" xmlns="http://www.w3.org/2000/svg" width="500.79" height="369.509" viewBox="0 0 500.79 369.509">
+            <g transform="translate(-349.185 -228.221)">
+                <path d="M98.9-143.6c42,16.6,97.6,22.6,127,51,29.5,28.5,33,79.4,23.1,126.4s-33.1,90.1-68.6,111.8-83.3,22-123.3,21.2c-40.1-.8-72.4-2.7-116.3.2-43.8,2.9-99.3,10.6-128.9-11-29.7-21.6-33.6-72.6-43.3-124.2S-256.5-72-238.5-107.8c18.1-35.9,69.6-55.3,115-70.4,45.3-15.2,84.4-26,118.2-17.7S56.9-160.3,98.9-143.6Z" transform="translate(596 427)"/>
+            </g>
+        </svg>
     </section>
 
 @endsection
